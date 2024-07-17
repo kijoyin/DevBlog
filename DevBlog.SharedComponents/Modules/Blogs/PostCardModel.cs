828a67eab4f24bad15cb88e1ca9db404ea40344a
Kiran Joy <kijoyin@gmail.com>
2024-07-17 12:17:30 +1000

    Adding some post lis

---
+++ DevBlog.SharedComponents/Modules/Blogs/PostCardModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevBlog.SharedComponents.Modules.Blogs
+{
+    public class PostCardModel(string title,string? subtitle,string summary,string slug)
+    {
+        
+        public string Title { get; set; } = title;
+        public string? SubTitle { get; set; } = subtitle;
+        public string Summary { get; set; } = summary;
+        public string Slug { get; set; } = slug;
+    }
+}
